c8c55fa260381bef593c7eaf6dfadea7eb298d47
SandeshMattam <77489240+SandeshMattam@users.noreply.github.com>
2021-02-10 08:38:05 +0530

    Create Animation1.java

---
+++ Animation1.java
@@ -0,0 +1,38 @@
+public class Animation1 {
+    /* Hello every body in this video i am typing on a mechanical keyboard 
+    and making a wave animation in java using ASCII , not wave but any other 
+    results i will get , let me just get the value of how much to sleep from 
+    another file ok it is 100 , thats it for this video guys i will just end 
+    task in task manager so the my computer wont die:) , bye guys*/
+    public static void main(String[] args) throws InterruptedException {
+
+        int j = 100;
+
+        while(true){
+            System.out.println("----------");
+
+            Thread.sleep(j);
+            
+            System.out.println(" --------");
+
+            Thread.sleep(j);
+
+            System.out.println("  ------");
+
+            Thread.sleep(j);
+
+            System.out.println("   ----");
+
+            Thread.sleep(j);
+
+            System.out.println("    --");
+
+            Thread.sleep(j);
+
+            System.out.println("     -");
+
+            Thread.sleep(j);
+
+            }
+    }
+}
